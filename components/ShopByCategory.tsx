f661de8936bd6af9f847699f3402e59e21357098
NB-66-08-02\NB-66-08-02 <savaddee159@gmail.com>
2025-06-10 16:43:58 +0700

    feat: create layout shopByCategory

--- components/ShopByCategory.tsx
+++ components/ShopByCategory.tsx
@@ -1,26 +1,37 @@
 const ShopByCategory = () => {
   return (
-    <section className="px-6 py-16 bg-white">
-      <h2 className="text-2xl md:text-3xl font-bold mb-8">Shop by Category</h2>
-      <div className="grid grid-cols-2 gap-4">
+    <section className="px-6 py-16 bg-white max-w-7xl mx-auto  ">
+      <h2 className="text-2xl md:text-3xl font-bold mb-8 text-center">
+        Shop by Category
+      </h2>
+      <div className="grid grid-cols-1 md:grid-cols-4 gap-4 bg-red-400 ">
         <img
           src="/images/t-shirt-category.jpg"
           alt=""
-          className="row-span-2 h-80"
+          className=" w-full h-full object-cover md:col-span-2 "
         />
-        <div className="grid gap-4">
-          <img src="/images/t-shirt-category.jpg" alt="" className="h-80" />
-          <img src="/images/t-shirt-category.jpg" alt="" className="h-80" />
-        </div>
-      </div>
-      {/* <div className="mt-6 text-right">
+        <img
+          src="/images/t-shirt-category.jpg"
+          alt=""
+          className="w-full h-full object-cover"
+        />
+        <img
+          src="/images/t-shirt-category.jpg"
+          alt=""
+          className="w-full h-full object-cover"
+        />
+        <img
+          src="/images/t-shirt-category.jpg"
+          alt=""
+          className=" w-full h-full object-cover  md:col-span-2"
+        />
         <a
           href="/products"
-          className="text-sm font-semibold text-black hover:underline inline-flex items-center gap-1"
+          className="text-sm font-semibold text-black hover:underline inline-flex items-center gap-1 justify-center md:col-span-2"
         >
           View All Products →
         </a>
-      </div> */}
+      </div>
     </section>
   );
 };
