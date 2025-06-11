c8716643401b604a9805e323c22a9fb912fdca03
NB-66-08-02\NB-66-08-02 <savaddee159@gmail.com>
2025-06-11 16:07:33 +0700

    feat: create form Login

--- app/(shop)/signup/page.tsx
+++ app/(shop)/signup/page.tsx
@@ -1,4 +1,12 @@
+import { Label } from "@/components/ui/label";
+
 const SignupPage = () => {
-  return <div>SignupPage</div>;
+  return (
+    <div className=" bg-gray-50 h-screen py-10 px-4 sm:px-6 lg:px-8 ">
+      <Label className="flex justify-center text-3xl">
+        Signup ไปดู Register ของ IhaveCPU
+      </Label>
+    </div>
+  );
 };
 export default SignupPage;
--- components/DialogLogin.tsx
+++ components/DialogLogin.tsx
@@ -12,60 +12,88 @@ import {
 } from "@/components/ui/dialog";
 import { Separator } from "./ui/separator";
 import Link from "next/link";
+import { FormProvider, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import FormInputField from "./FormInput/FormInputField";
+import { useState } from "react";
+
+const formSchema = z.object({
+  email: z.string().email("Invalid email format. Please enter a valid email."),
+  password: z.string().min(1, { message: "password is required." }),
+});
+
 const DialogLogin = () => {
+  const [open, setOpen] = useState(true);
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    mode: "onSubmit",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleSubmit = (data: z.infer<typeof formSchema>) => {
+    console.log("Login data", data);
+    // TODO: login logic here
+  };
   return (
-    <Dialog>
-      <form>
-        <DialogTrigger asChild className="cursor-pointer">
-          <Label>Sign In</Label>
-        </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
-          <DialogHeader>
-            <DialogTitle className="text-center">Sign In</DialogTitle>
-          </DialogHeader>
-          <div className="grid gap-4">
-            <div className="grid gap-3">
-              <Label htmlFor="email">Email</Label>
-              <Input
-                id="email"
-                name="email"
-                type="email"
-                placeholder="example@gmail.com"
-              />
-            </div>
-            <div className="grid gap-3">
-              <Label htmlFor="password">Username</Label>
-              <Input
-                id="password"
-                name="password"
-                placeholder="*********"
-                type="password"
-              />
-            </div>
-          </div>
-          <DialogFooter>
-            <div className="flex flex-col w-full gap-3">
-              <Button className="w-full" type="submit">
-                Login
-              </Button>
-            </div>
-          </DialogFooter>
-          <Separator />
-          <div className="flex items-center justify-center">
-            <Label className="text-center text-gray-500 font-normal">
-              No account ?
-              <DialogClose asChild>
-                <Link
-                  href="/signup"
-                  className="underline font-semibold text-black"
-                >
-                  SignUp
-                </Link>
-              </DialogClose>
-            </Label>
-          </div>
-        </DialogContent>
-      </form>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild className="cursor-pointer">
+        <Label>Sign In</Label>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[425px]">
+        <DialogHeader>
+          <DialogTitle className="text-center">Sign In</DialogTitle>
+        </DialogHeader>
+        <FormProvider {...form}>
+          <form
+            onSubmit={form.handleSubmit(handleSubmit)}
+            className="space-y-5"
+          >
+            <FormInputField
+              control={form.control}
+              name="email"
+              label="Email"
+              placeholder="you@example.com"
+            />
+            <FormInputField
+              control={form.control}
+              name="password"
+              label="Password"
+              type="password"
+              placeholder="*******"
+            />
+            {/* {isError && axios.isAxiosError(error) && (
+                <p className="text-red-500 text-sm ">
+                  {error?.response?.data?.error}
+                </p>
+              )} */}
+            <DialogFooter>
+              <div className="flex flex-col w-full gap-3">
+                <Button className="w-full" type="submit">
+                  Login
+                </Button>
+              </div>
+            </DialogFooter>
+          </form>
+        </FormProvider>
+        <Separator />
+        <div className="flex items-center justify-center">
+          <Label className="text-center text-gray-500 font-normal">
+            No account ?
+            <DialogClose asChild>
+              <Link
+                href="/signup"
+                className="underline font-semibold text-black"
+              >
+                SignUp
+              </Link>
+            </DialogClose>
+          </Label>
+        </div>
+      </DialogContent>
     </Dialog>
   );
 };
