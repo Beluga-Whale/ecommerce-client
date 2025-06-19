d073e9a688bf6c6591db4f86a159a3e92cfea24b
NB-66-08-02\NB-66-08-02 <savaddee159@gmail.com>
2025-06-19 15:40:55 +0700

    feat: update use redux toolkit persist

--- app/StoreProvider.tsx
+++ app/StoreProvider.tsx
@@ -1,18 +1,29 @@
 "use client";
+
 import { useRef } from "react";
 import { Provider } from "react-redux";
 import { makeStore, AppStore } from "../lib/store";
+import { persistStore } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 export default function StoreProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const storeRef = useRef<AppStore>(undefined);
+  const storeRef = useRef<AppStore | null>(null);
+  const persistorRef = useRef<ReturnType<typeof persistStore> | null>(null);
+
   if (!storeRef.current) {
-    // Create the store instance the first time this renders
     storeRef.current = makeStore();
+    persistorRef.current = persistStore(storeRef.current);
   }
 
-  return <Provider store={storeRef.current}>{children}</Provider>;
+  return (
+    <Provider store={storeRef.current}>
+      <PersistGate loading={null} persistor={persistorRef.current!}>
+        {children}
+      </PersistGate>
+    </Provider>
+  );
 }
--- lib/store.ts
+++ lib/store.ts
@@ -1,17 +1,46 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { combineReducers } from "redux";
 import dialogReducer from "./features/dialog/dialogSlice";
 import filterReducer from "./features/filter/filerSlice";
 import cartReducer from "./features/cart/cartSlice";
 
+const rootReducer = combineReducers({
+  dialog: dialogReducer,
+  filter: filterReducer,
+  cart: cartReducer,
+});
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 export const makeStore = () => {
   return configureStore({
-    reducer: {
-      dialog: dialogReducer,
-      filter: filterReducer,
-      cart: cartReducer,
-    },
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        serializableCheck: {
+          ignoredActions: [
+            "persist/PERSIST",
+            "persist/REHYDRATE",
+            "persist/PAUSE",
+            "persist/FLUSH",
+            "persist/PURGE",
+            "persist/REGISTER",
+          ],
+        },
+      }),
   });
 };
+
+export const store = makeStore();
+export const persistor = persistStore(store);
 
 // Infer the type of makeStore
 export type AppStore = ReturnType<typeof makeStore>;
